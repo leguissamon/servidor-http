f7a473cdb388d16edb257ff207e68b498c802eb6
leguissamon <leguissamonk@gmail.com>
2026-08-14 21:26:46 +0000

    core: added script for routes

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,17 +1,60 @@
-import http from 'node:http';
+import http from 'node:http'
+import { URL } from 'node:url'
 
 const porta = 3000
 
 const server = http.createServer();
 
-server.on('request', (req, res) =>{
-    console.log(`servidor funcionando! ${req.method} ${req.url}`);
-    console.log(new Date().toISOString())
-    res.statusCode = 201
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    res.end(JSON.stringify({status: "ok"}));
-});
 
-server.listen(porta, ()=> {
+const requisicao = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200
+     const urlObj = new URL(req.url, `http://${req.headers.host}`);
+
+    if (req.method === 'GET' && urlObj.pathname === '/saudacao'){
+        const nome = urlObj.searchParams.get('nome');
+        return res.end(JSON.stringify({"nome" : nome}));
+    
+    }
+    
+    else if (req.method === 'GET' && urlObj.pathname === '/'){
+        console.log(`Requisição recebida! ${req.method} ${req.url}`);
+        return res.end(JSON.stringify({"data" : "está é a pagina inicial"}));
+    
+    }
+    
+    else if (req.method === 'GET' && urlObj.pathname === '/contato'){
+        console.log(`Requisição recebida! ${req.method} ${req.url}`);
+        return res.end(JSON.stringify({"data" : [{"telefone" : "67 99999999" }, {"email": "email@gmail.com"}]}));
+    
+    }
+    
+    else if (req.method === 'GET' && urlObj.pathname === '/produto'){
+        console.log(`Requisição recebida! ${req.method} ${req.url}`);
+        return res.end(JSON.stringify(
+            {"data" : [{"descricao" : "nome do produto" }, {"preco": "xxxxxx"}]}));
+    
+    }
+    
+    else if (req.method === 'GET' && urlObj.pathname === '/status'){
+        console.log(`Requisição recebida! ${req.method} ${req.url}`);
+        return res.end(JSON.stringify({ "status": "ok" }));
+    }
+   
+
+     res.statusCode = 404;
+     return res.end(JSON.stringify({"erro" : "Nenhuma rota foi encontrada!! verifique o endereço de acesso."}));
+
+
+    return res.end(JSON.stringify({ "chave": "valor" }));
+
+
+    console.log(`Requisição recebida! ${req.method} ${req.url}`);
+    res.end();
+}
+
+server.on('request', requisicao);
+
+server.listen(porta, () => {
     console.log(`Servidor ouvindo na porta ${porta}`)
-});
+});			
